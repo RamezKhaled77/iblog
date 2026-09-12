4b0641b9625bb879bddb0d5c6e31ba5bd81541c5
Ramez Khaled <ramez.elzoz74@gmail.com>
2026-09-13 00:10:25 +0300

    create the favicon & metadata

--- app/(shared-layout)/page.tsx
+++ app/(shared-layout)/page.tsx
@@ -1,3 +1,66 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "iBlog — Modern Blogging Platform",
+    template: "%s | iBlog",
+  },
+  description:
+    "Discover inspiring articles, tech insights, and creative stories on iBlog. Join our community of readers and writers today.",
+
+  keywords: [
+    "iBlog",
+    "Blog",
+    "Tech Articles",
+    "Software Engineering",
+    "Web Development",
+    "Writing Platform",
+    "Blog Posts",
+  ],
+
+  authors: [{ name: "Ramez Khaled" }],
+  creator: "iBlog",
+  publisher: "iBlog",
+
+  openGraph: {
+    title: "iBlog — Modern Blogging Platform",
+    description:
+      "Discover inspiring articles, tech insights, and creative stories on iBlog.",
+    url: "https://iblog-eosin.vercel.app",
+    siteName: "iBlog",
+    images: [
+      {
+        url: "https://iblog-eosin.vercel.app/icon.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "iBlog Preview Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "iBlog — Modern Blogging Platform",
+    description:
+      "Discover inspiring articles, tech insights, and creative stories on iBlog.",
+    images: ["https://iblog-eosin.vercel.app/og-image.png"],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
+
 export default function Home() {
   return <div>index page</div>;
 }
